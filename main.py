41871cae70eecc8872ca0e264442dc5c30888e7e
Jamie Zhao <bluewolfJ124@gmail.com>
2023-06-10 09:44:49 +1000

    uses directory to find templates

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import cv2
 import pyautogui
 import time
 import numpy as np
+import os
+templates = os.listdir('templates')
 def read():
     file = pyautogui.screenshot(region=(550,200, 600, 250))
     x_val = []
     numbers = []
     img_rgb = cv2.cvtColor(np.array(file), cv2.COLOR_RGB2BGR)
-    for i in range(10): # Identify numbers
+    for i in range(len(templates)): # Identify numbers
         template = cv2.imread("templates\\"+str(i)+'.png')
 
         res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
         threshold = .8
         loc = np.where(res >= threshold)
-        w, h = template.shape[:-1]
-        for pt in zip(*loc[::-1]):  # Switch columns and rows
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
         if (loc[::-1])[0].size and (loc[::-1][1]).size > 4:
             print(i)
             x_val.append(np.array(loc[::-1][0]).tolist())
             numbers.append(i)
-    cv2.imwrite('result.png', img_rgb)
     print(x_val)
     print(numbers)
 
@@ -38,5 +36,3 @@ time.sleep(2)
 while True:
     read()
     pyautogui.press("enter")
-
-
