890b655df75a3b2f9354647a90673c5164f1f635
Jamie Zhao <bluewolfJ124@gmail.com>
2023-06-10 10:53:01 +1000

    properly implimented the addition symbol detection

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import time
 import numpy as np
 import os
 templates = os.listdir('templates')
+print(templates)
 def read():
     file = pyautogui.screenshot(region=(550,200, 600, 250))
     x_val = []
     numbers = []
     img_rgb = cv2.cvtColor(np.array(file), cv2.COLOR_RGB2BGR)
-    for i in range(len(templates)): # Identify numbers
-        template = cv2.imread("templates\\"+str(i)+'.png')
+    for i in templates: # Identify numbers
+        template = cv2.imread('templates\\'+i)
 
         res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
         threshold = .8
@@ -18,19 +19,23 @@ def read():
         if (loc[::-1])[0].size and (loc[::-1][1]).size > 4:
             print(i)
             x_val.append(np.array(loc[::-1][0]).tolist())
-            numbers.append(i)
-    print(x_val)
+            numbers.append(os.path.splitext(i)[0])
     print(numbers)
-
     for i in x_val:
-        x_val[x_val.index(i)] = max(i)
-
-    Z = [x for _,x in sorted(zip(x_val,numbers))]
+        if type(i) == int:
+            pass
+        else:
+            if max(i) - min(i) > 100:
+                numbers.append(numbers[x_val.index(i)])
+                x_val[x_val.index(i)] = max(i)
+                x_val.append(min(i))
+                print("repeated")
+            else:
+                x_val[x_val.index(i)] = max(i)
     print(x_val)
-    if len(Z) == 1:
-        pyautogui.write(str(Z[0]*2),interval=0)
-    else:
-        pyautogui.write(str(Z[0]+Z[1]),interval=0)
+    Z = [x for _,x in sorted(zip(x_val,numbers))]
+    pyautogui.write(str(eval(str(''.join(Z)))),interval=0)
+
 time.sleep(2)
     
 while True:
